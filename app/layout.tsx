d3e6285bd9355f7e5a867837b20de494d67aefb7
hr82al <hr82al@gmail.com>
2023-08-26 13:50:59 +0300

    Initial commit from Create Next App

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+
+const poppins = Poppins({weight: "400", subsets: ['latin']});
+
+export const metadata: Metadata = {
+  title: 'Quiz Learn',
+  description: 'Quiz application to learn typescript and other languages',
+  keywords: 'learn, TS, typescript, quiz',
+  authors : {name: 'Alexandr Khromov'},
+  creator: 'Alexandr Khromov',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  )
+}
---
+++ components/card/Card.tsx
@@ -0,0 +1,12 @@
+
+export interface CardInterface {
+  id: number,
+  question: string,
+  answers: string[],
+}
+
+export default function Card({id}: {id: number}) {
+  return (
+    <p>d</p>
+  )
+}
