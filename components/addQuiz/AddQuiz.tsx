70f498b5caec2c64397863b95f1f7e23f0dfb315
hr82al <hr82al@gmail.com>
2023-09-28 07:37:25 +0300

    Some improvment of add quiz.

--- components/addQuiz/AddQuiz.tsx
+++ components/addQuiz/AddQuiz.tsx
@@ -1,54 +1,59 @@
 import { JetBrains_Mono } from "next/font/google";
 import Navbar from "../Navbar";
-import { use, useMemo, useState } from "react";
-import { prettyQuiz, splitToItems } from "@/quiz/utils";
-import { hlog } from "../prisma";
-import { QuizEnum } from "@prisma/client";
+import { useEffect, useState } from "react";
+
 
 const jetBrainFont = JetBrains_Mono({ subsets: ["cyrillic-ext"] });
 
 enum QuizButtonTextEnum {
   Question = "Question",
   Quiz = "Quiz",
-  CodeQuiz = "Code Quiz",
   Answer = "Answer",
-  CodeAnswer = "Code Answer"
 }
 
 
 
 export default function AddQuiz() {
   const [text, setText] = useState("");
-  const [question, setQuestion] = useState<string>("How to ...");
-  const [quiz, setQuiz] = useState<string>("");
-  const [answer, setAnswer] = useState<string>("");
+  const [question, setQuestion] = useState("How to ...");
+  const [quiz, setQuiz] = useState(`function greet(person: { name: string; age: number }) {
+    return "Hello " + person.name;
+}`);
+  const [answer, setAnswer] = useState(quiz);
   const [buttonText, setButtonText] = useState<QuizButtonTextEnum>(QuizButtonTextEnum.Question);
 
   
   const isReady = question.length !== 0 && quiz.length !== 0 && answer.length !== 0;
 
-  const quiz_code = useMemo(() => {
-    return JSON.stringify(splitToItems(quiz));
-  }, [quiz]);
+  useEffect(() => {
+    switch (buttonText) {
+      case QuizButtonTextEnum.Question:
+        setText(question);
+        break;
+      case QuizButtonTextEnum.Quiz:
+        setText(quiz);
+        break;
+      case QuizButtonTextEnum.Answer:
+        setText(answer);
+    }
+  }, [buttonText]);
 
   function handleAction() {
     const allText = Object.values(QuizButtonTextEnum)
     setButtonText(
       allText[(allText.indexOf(buttonText) + 1) % allText.length]
     )
+
     switch (buttonText) {
       case QuizButtonTextEnum.Question:
         setQuestion(text);
-        setText(quiz);
         break;
       case QuizButtonTextEnum.Quiz:
         setQuiz(text);
-        setText(quiz_code);
-        break
-      case QuizButtonTextEnum.CodeQuiz:
-        setText(answer);
+        break;;
+      case QuizButtonTextEnum.Answer:
+        setAnswer(text);
     }
-    // if btnN is 1 then next is Raw quiz 
   }
 
   return (
@@ -64,9 +69,10 @@ export default function AddQuiz() {
           value={text}
         >
         </textarea>
+        
         <div className="flex justify-end gap-2">
           <button 
-            className="btn w-36"
+            className="btn w-28"
             onClick={handleAction}
           >
             {buttonText.valueOf()}
