f2f6e6edfefe1baec1d630795a3faf0790eed7e0
hr82al <hr82al@gmail.com>
2023-10-11 12:16:28 +0300

    Config jest

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,3 @@
 export { default } from "next-auth/middleware";
 
-export const config = { matcher: [
-  "/learn",
-  "/admin/quiz", 
-  "/admin/quiz_list",
-  "/api/admin",
-] };
+// export const config = { matcher: [] };
