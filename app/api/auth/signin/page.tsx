764f043f1ad072629b5052f12e7cfe6a41573795
hr82al <hr82al@gmail.com>
2023-10-17 17:35:45 +0300

    fix: a login page problem with empty autofilled values

--- app/api/auth/signin/page.tsx
+++ app/api/auth/signin/page.tsx
@@ -1,27 +1,41 @@
 "use client"
-import { signIn } from "next-auth/react";
-import { useRouter } from "next/navigation";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useRef } from "react";
 import Image from "next/image"
 
-export default function Login({ searchParams }: { searchParams: { callbackUrl: string } }) {
-  const username = useRef("");
-  const password = useRef("");
+export default function Login() {
+  const username = useRef<HTMLInputElement>(null);
+  const password = useRef<HTMLInputElement>(null);
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl") ?? "/";
+  const error = searchParams.get("error") ?? "";
   const router = useRouter();
+  const session = useSession();
+
+  const errorUI = (error.length > 0 ? (
+    <div className="text-emerald-100 text-center">Error: {error}</div>
+  ) : (
+    <div className="text-main-base text-opacity-0">hidden </div>
+  ));
+
+  if (session.data) {
+    router.push(callbackUrl);
+  }
 
   async function handleSubmit() {
     const result = await signIn("credentials", {
-      username: username.current,
-      password: password.current,
+      username: username.current?.value,
+      password: password.current?.value,
       redirect: true,
-      callbackUrl: searchParams.callbackUrl,
+      callbackUrl: callbackUrl,
     });
   }
 
   async function handleGoogleSubmit() {
     const result = await signIn("google", {
       redirect: true,
-      callbackUrl: searchParams.callbackUrl
+      callbackUrl: callbackUrl,
     });
   }
 
@@ -36,13 +50,15 @@ export default function Login({ searchParams }: { searchParams: { callbackUrl: s
           Login with Google
         </button>
 
-        <hr className="border-main-darkest my-6"/>
+        <hr className="border-main-darkest mt-6 mb-2"/>
+
+        {errorUI}
 
         <label className="block rounded-md" htmlFor="register-user">User:</label>
-        <input maxLength={25} className="block mb-2 rounded-md quiz-input" id="register-user" type="text" name="register-user" onChange={e => { username.current = e.target.value }} autoFocus spellCheck={false} />
+        <input ref={username} maxLength={25} className="block mb-2 rounded-md quiz-input" id="register-user" type="text" name="register-user"  autoFocus spellCheck={false} />
 
         <label className="block rounded-md" htmlFor="register-password">Password:</label>
-        <input maxLength={40} className="block rounded-md quiz-input mb-3" id="register-password" type="password" name="register-password" onChange={e => { password.current = e.target.value }} />
+        <input ref={password} maxLength={40} className="block rounded-md quiz-input mb-3" id="register-password" type="password" name="register-password" />
 
         <button className="btn mb-3" onClick={handleSubmit}>
           Login
